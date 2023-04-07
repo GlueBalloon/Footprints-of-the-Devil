7b2510b753744501e5c87a5ce0414a0313643516
Jesse Wonder Clark <jwc@jessewonderclark.com>
2023-04-07 13:39:35 -0400

    perlin map with random magnification

--- Map.lua
+++ Map.lua
@@ -10,49 +10,25 @@ function Map:init()
     self.terrain = {}
     self.units = {}
     self.cellColors = {}
-    self.fakePixelsPerSide = 3
+    self.fakePixelsPerSide = 4
     self.smallCellSize = self.cellSize / self.fakePixelsPerSide -- Calculate the size of the smaller rects
     
-
-    
     self.smallCellColors = {}
-    local noiseScale = 0.975
-    local noiseMaker = craft.noise.perlin()
-    
-
-    local offset = 200
-    for i = 1, self.gridSize do
-        self.smallCellColors[i] = {}
-        for j = 1, self.gridSize do
-            self.smallCellColors[i][j] = {}
-            for k = 1, self.fakePixelsPerSide do
-                self.smallCellColors[i][j][k] = {}
-                for l = 1, self.fakePixelsPerSide do
-                    local noiseValue = noiseMaker:getValue(
-                    (i * noiseScale) + (l * offset),
-                    (j * noiseScale) + (l * offset),
-                    (k * noiseScale) + (l * offset)
-                    )
-                    local t = (noiseValue + 1) / 2 -- Normalize the noise value to [0, 1]
-                    self.smallCellColors[i][j][k][l] = self:lerpColorBeigeToRust(t)
-                end
-            end
-        end
-    end
-
-    rectWidth = 360
-    rectHeight = 360
-    rectX = (WIDTH - rectWidth) / 2
-    rectY = (HEIGHT - rectHeight) / 2
-    perlinNoise = craft.noise.perlin()
-    offsetX = math.random(0, 10000)
-    offsetY = math.random(0, 10000)
-    
+    --color pairs
+    self.highestElevationColor = color(245, 222, 179)
+    self.lowestElevationColor = color(226, 150, 117)
+    --or
+    self.highestElevationColor = color(217, 203, 171)
+    self.lowestElevationColor = color(211, 150, 124)
+    --or
+    self.highestElevationColor = color(219, 169, 138)
+    self.lowestElevationColor = color(192, 107, 68)
     self.perlinNoise = craft.noise.perlin()
     self.offsetXNoise = math.random(0, 10000)
     self.offsetYNoise = math.random(0, 10000)
-    
-    local noiseScale = 0.01
+    local noiseMagnifier = math.random(65, 130) * 0.01
+    print(noiseMagnifier)
+    self.noiseScale = 0.0075 * noiseMagnifier
     for i = 1, self.gridSize do
         self.smallCellColors[i] = {}
         for j = 1, self.gridSize do
@@ -61,42 +37,32 @@ function Map:init()
                 self.smallCellColors[i][j][k] = {}
                 for l = 1, self.fakePixelsPerSide do
                     local noiseValue = self.perlinNoise:getValue(
-                    (self.offsetX + (i - 1) * self.cellSize + (k - 1) * self.smallCellSize + self.offsetXNoise) * noiseScale, 
-                    (self.offsetY + (j - 1) * self.cellSize + (l - 1) * self.smallCellSize + self.offsetYNoise) * noiseScale, 
+                    (self.offsetX + (i - 1) * self.cellSize + (k - 1) * self.smallCellSize + self.offsetXNoise) * self.noiseScale, 
+                    (self.offsetY + (j - 1) * self.cellSize + (l - 1) * self.smallCellSize + self.offsetYNoise) * self.noiseScale, 
                     0
                     )
-                    self.smallCellColors[i][j][k][l] = self:lerpColorBeigeToRust(noiseValue)
+                    -- Normalize the noiseValue to be between 0 and 1
+                    noiseValue = (noiseValue + 1) / 2
+                    noiseValue = math.max(0, math.min(1, noiseValue))
+                    self.smallCellColors[i][j][k][l] = self:lerpColors(noiseValue, self.highestElevationColor, self.lowestElevationColor)
                 end
             end
         end
     end
+    parameter.boolean("drawMiniNoise")
+    parameter.color("highColor", color(243, 242, 241))
+    parameter.color("lowColor", color(211, 150, 124))
+    highColor = self.highestElevationColor
+    lowColor = self.lowestElevationColor
 end
 
-function Map:drawPerlinNoisePattern(x, y, width, height, squareSize, noiseGenerator)
-    local scaleFactor = 0.01 -- Controls the "zoom" level of the noise
-    for i = x, x + width, squareSize do
-        for j = y, y + height, squareSize do
-            local noiseValue = noiseGenerator:getValue((i + self.offsetXNoise) * scaleFactor, (j + self.offsetYNoise) * scaleFactor, 0)
-            fill(Map:lerpColorBeigeToRust(noiseValue))
-            rect(i, j, squareSize, squareSize)
-        end
+function Map:lerpColors(amount, color1, color2)
+    function lerp(a, b, amount)
+        return a + (b - a) * amount
     end
-end
-
-
-
-function Map:lerpColorBeigeToRust(t)
-    function lerp(a, b, t)
-        return a + (b - a) * t
-    end
-    local beige = color(245, 222, 179)
-    local rust = color(183, 65, 14)
- --local beige = color(245, 245, 220)
- --   local rust = color(226, 190, 165)
-    local rust = color(226, 150, 117)
-    local r = lerp(beige.r, rust.r, t)
-    local g = lerp(beige.g, rust.g, t)
-    local b = lerp(beige.b, rust.b, t)
+    local r = lerp(color1.r, color2.r, amount)
+    local g = lerp(color1.g, color2.g, amount)
+    local b = lerp(color1.b, color2.b, amount)
     local returnColor = color(r, g, b)
     return returnColor
 end
@@ -132,7 +98,10 @@ function Map:draw()
         line(self.offsetX, self.offsetY + i * self.cellSize, self.offsetX + self.gridSize * self.cellSize, self.offsetY + i * self.cellSize)
     end
     popStyle()
-   self: drawPerlinNoisePattern(rectX, rectY, rectWidth, rectHeight, 10, self.perlinNoise)
+    
+    if drawMiniNoise then
+        self:drawPerlinNoisePattern()
+    end
 end
 
 
@@ -163,5 +132,27 @@ function Map:randomColorBetweenBeigeAndRust()
     return color(r, g, b)
 end
 
-
+function Map:drawPerlinNoisePattern(x, y, width, height, squareSize, noiseGenerator)
+    x = x or self.offsetX + (self.smallCellSize * 0.7)
+    y = y or self.offsetY - (self.smallCellSize * 0.3)
+    squareSize = squareSize or self.smallCellSize * 0.967697
+    width = width or self.gridSize * squareSize * self.fakePixelsPerSide
+    height = height or width
+    noiseGenerator = noiseGenerator or self.perlinNoise
+    fill(32, 51, 100, 25)
+    rect(x - 2, y - 2, (width * 1.025) + 1, (height * 1.025) + 1)
+    self.highestElevationColor = highColor
+    self.lowestElevationColor = lowColor
+    local scaleFactor = self.noiseScale -- Controls the "zoom" level of the noise
+    for i = x, x + width, squareSize do
+        for j = y, y + height, squareSize do
+            local noiseValue = noiseGenerator:getValue((i + self.offsetXNoise) * scaleFactor, (j + self.offsetYNoise) * scaleFactor, 0)
+            -- Normalize the noiseValue to be between 0 and 1
+            noiseValue = (noiseValue + 1) / 2
+            noiseValue = math.max(0, math.min(1, noiseValue))
+            fill(Map:lerpColors(noiseValue, self.highestElevationColor, self.lowestElevationColor))
+            rect(i, j, squareSize, squareSize)
+        end
+    end
+end
 
